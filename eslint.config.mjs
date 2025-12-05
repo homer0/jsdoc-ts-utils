cd9a35ca0a51418dcff6a4c5e413f9f61136217b
homer0 <me@homer0.dev>
2025-12-05 05:46:07 +0100

    chore: update eslint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from 'eslint/config';
+import { createConfig } from '@homer0/eslint-plugin/create';
+
+export default defineConfig([
+  createConfig({
+    importUrl: import.meta.url,
+    ignores: ['tests/**', 'docs/**', '.prettierrc.mjs'],
+    configs: ['node-with-prettier', 'jsdoc'],
+    addTsParser: false,
+    extraneousDependencies: {
+      devFiles: ['vitest.*.config.js'],
+    },
+    sourceType: 'commonjs',
+    rules: {
+      'jsdoc/check-tag-names': [
+        'error',
+        {
+          definedTags: ['parent', 'prettierignore'],
+        },
+      ],
+    },
+  }),
+  {
+    ignores: ['tests/e2e/fixtures/*.js'],
+  },
+  createConfig({
+    importUrl: import.meta.url,
+    files: 'all-inside:./tests',
+    configs: ['node-with-prettier', 'tests'],
+    addTsParser: false,
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'commonjs',
+      },
+      globals: {
+        it: 'readonly',
+        describe: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        jest: 'readonly',
+      },
+    },
+    rules: {
+      'no-use-before-define': 'off',
+    },
+  }),
+]);
